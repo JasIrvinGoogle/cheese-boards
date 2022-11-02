e1d58fc92fd72847b8a4e7d0cf69d6bae8d870bb
JasIrvin <jasmineirvin@google.com>
2022-11-02 12:05:16 -0400

    started seedData - added cheese

---
+++ seedData.js
@@ -0,0 +1,27 @@
+const cheeseData = [
+    {
+        title: 'American', 
+        description: 'American is a creamy, smooth cheese made from blending natural cheeses. It comes in several forms including individually wrapped cheese slices, small pre-sliced blocks and large blocks. It melts well.'
+    }, 
+
+    {
+        title: 'Asiago',
+        description: 'Asiago, a nutty-flavored cheese, comes in two forms: fresh and mature. The fresh has an off-white color and is smoother and milder, while mature Asiago is yellowish and somewhat crumbly. Depending on its age, Asiago can be grated, melted or sliced.'
+    }, 
+
+    {
+        title: 'Brie',
+        description: 'Brie is a soft, white cheese. It comes in a wheel, sometimes in a small wooden box, and is considered a great dessert cheese. Experts recommend enjoying it at room temperature.'
+    },
+
+    {
+        title: 'Burrata',
+        description: 'Burrata is a fresh cheese featuring a thin layer of cheese with a mixture of stringy curd and fresh cream on the inside. It has a rich flavor and goes well with salads, crusty bread and Italian dishes.'
+    }
+]
+
+module.exports = {
+    userData,
+    cheeseData,
+    boardData
+};
