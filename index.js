942950365aea34609f9ef0530e19d65808744a36
JasIrvin <jasmineirvin@google.com>
2022-11-02 11:47:15 -0400

    added some associations - unfinished

---
+++ index.js
@@ -0,0 +1,16 @@
+const { User } = require('./models/User');
+const { Board } = require('./models/Board'); 
+const { Cheese } = require('./models/Cheese'); 
+
+//Associations - one user can have many boards, one board can have many cheeses, cheese can be on multiple boards
+
+User.hasMany(Board) // one user to many boards
+Board.belongsTo(User) //user required to have a board - board has to belong to a user
+
+
+
+module.exports = {
+    User, 
+    Board, 
+    Cheese
+}
