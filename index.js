dc03c8651da95fa53f1dc9be2de3f977f02a4667
JasIrvin <jasmineirvin@google.com>
2022-11-16 11:31:02 -0500

    fixed associations - tests now run again

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const { Cheese } = require('./models/Cheese');
 
 //Associations - one user can have many boards, one board can have many cheeses, cheese can be on multiple boards
 
-User.hasMany(Board) // one user to many boards
-Board.belongsTo(User) //user required to have a board - board has to belong to a user
-Cheese.belongsToMany(Board) //cheese can be on multiple boards - unsure if this is correct
-
+//One to Many 
+User.hasMany(Board); // one user to many boards
+Board.belongsTo(User); //user required to have a board - board has to belong to a user
+ 
+//Many to Many 
+Board.belongsToMany(Cheese, {through: 'CheeseBoard'});
+Cheese.belongsToMany(Board, {through: 'CheeseBoard'})
 
 
 module.exports = {
